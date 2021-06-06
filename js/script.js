5f703264cb1042f0428be35c60f6692bd06d9e77
pr0pr3 <salut@tchang.fr>
2021-06-07 00:12:09 +0200

    create deck of 52 cards

--- js/script.js
+++ js/script.js
@@ -1,28 +1,38 @@
 class Card {
-    constructor(rank,suit, value){
+    constructor(rank,suit){
     this.rank = rank,
     this.suit = suit
-    this.value = value,
   }
 }
 
-var blackJack = new Card ("J","♠",10);
 var suits = ["♠","♥","♦","♣"] ;
-var rank = ["K","Q","J","10","9","8","7","6","5","4","3","2","1"];
+var ranks = ["K","Q","J","10","9","8","7","6","5","4","3","2","1"];
 var deck = [] /*52 cards*/
-var shoe = [] /*6*52 shuffled cards t*/
+for (var suit of suits){
+  for (var rank of ranks){
+    var i = new Card(rank,suit)
+    console.log(i);
+    deck.push(i);
+  }
+}
+
+console.log(deck);
+
+var shoe = [] //6*52 shuffled cards
 var playerScore = 0;
 var casinoScore = 0;
 var cash= 1000;
 var cardCount; /**/
 var bet;
+/*
 function makeBet() ;
 function shuffle();
 function showCard;
-function hit() ; /*draws a card */
+function hit() ; //draws a card
 function playerPlay();
 function casinoPlay();
 function pay();
-function setGame(); /*draw cards for both casino and player then calculate and show player's score */
-function runGame(); /* bet set hit playerPlay casinoPlay pay*/
-/*if cardCount > 52*2 run a turn else shuffle then run a game*/
+function setGame(); //draw cards for both casino and player then calculate and show player's score
+function runGame(); // bet set hit playerPlay casinoPlay pay
+//if cardCount > 52*2 run a turn else shuffle then run a game
+*/
